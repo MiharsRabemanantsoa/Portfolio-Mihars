08dfcc23868a9c092eb3af644217ec7066734429
MiharsRabemanantsoa <mihars.rabemanantsoa@gmail.com>
2022-07-07 13:09:38 +0300

    first commit

---
+++ forms/contact.php
@@ -0,0 +1,27 @@
+<?php
+
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'contact@example.com';
+
+  if( file_exists($php_email_form = '..vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_nom = $_POST['nom'];
+  $contact->from_email = $_POST['email'];
+  $contact->sujet = $_POST['sujet'];
+
+
+  $contact->add_message( $_POST['nom'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
